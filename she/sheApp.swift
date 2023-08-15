13666159a8b67962ffddae15b20a1fd897663f1d
kashvi <kashviswami@gmail.com>
2023-08-15 10:01:49 -0700

    Initial Commit

---
+++ she/sheApp.swift
@@ -0,0 +1,17 @@
+//
+//  sheApp.swift
+//  she
+//
+//  Created by Kashvi Swami on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct sheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
