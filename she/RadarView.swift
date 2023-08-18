58f8e4e8999fc039e9e1a853ad3264a7c40b14b6
kashvi <kashviswami@gmail.com>
2023-08-18 09:53:16 -0700

    ritual

--- she/RadarView.swift
+++ she/RadarView.swift
@@ -3,11 +3,60 @@
 import Foundation
 import SwiftUI
 
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
 struct RadarView: View {
+    @State private var toDoItems: [ToDoItem] = []
+    @State private var newItemName: String = ""
+
     var body: some View {
         NavigationStack {
-            Image("background").ignoresSafeArea()
+            ZStack {
+                Image("background").ignoresSafeArea()
+
+                VStack(spacing: 20) {
+                    Text("To-Do List")
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                        .padding()
+
+                    HStack {
+                        TextField("Enter new item", text: $newItemName)
+                            .padding(10)
+                            .background(Color.white.opacity(0.1))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+
+                        Button(action: {
+                            if newItemName.trimmingCharacters(in: .whitespaces) != "" {
+                                toDoItems.append(ToDoItem(name: newItemName))
+                                newItemName = ""
+                            }
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                                .padding(.leading, 10)
+                        }
+                    }
+
+                    List {
+                        ForEach(toDoItems) { item in
+                            Text(item.name)
+                        }
+                        .onDelete(perform: removeItems)
+                    }
+                    .padding()
+                }
+            }
         }
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        toDoItems.remove(atOffsets: offsets)
     }
 }
 
@@ -16,4 +65,3 @@ struct RadarView_Previews: PreviewProvider {
         RadarView()
     }
 }
-
--- she/RitualView.swift
+++ she/RitualView.swift
@@ -1,15 +1,41 @@
 import SwiftUI
+import Combine
 
-struct Task: Identifiable {
+struct Task: Identifiable, Codable {
     var id = UUID()
     var name: String
     var time: String
 }
 
-struct TaskList: Identifiable {
+struct TaskList: Identifiable, Codable {
     var id = UUID()
     var name: String
     var tasks: [Task]
+}
+
+class TaskListManager: ObservableObject {
+    @Published var taskLists: [TaskList] = {
+        if let data = UserDefaults.standard.data(forKey: "taskLists"), let savedTaskLists = try? JSONDecoder().decode([TaskList].self, from: data) {
+            return savedTaskLists
+        } else {
+            return [TaskList(name: "Morning", tasks: [
+                        Task(name: "Stretch", time: "8:00-8:10am"),
+                        Task(name: "Shower", time: "8:10-8:40am")
+                    ])]
+        }
+    }()
+    
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        cancellable = $taskLists
+            .debounce(for: 2.0, scheduler: RunLoop.main)
+            .sink { lists in
+                if let data = try? JSONEncoder().encode(lists) {
+                    UserDefaults.standard.set(data, forKey: "taskLists")
+                }
+            }
+    }
 }
 
 struct TaskRowView: View {
@@ -17,18 +43,18 @@ struct TaskRowView: View {
 
     var body: some View {
         HStack {
-            TextField("task", text: $task.name)
+            TextField("Task", text: $task.name)
                 .autocapitalization(.none)
                 .padding()
                 .cornerRadius(8)
                 .foregroundColor(.white)
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            TextField("time", text: $task.time)
+                .multilineTextAlignment(.center)
+            TextField("Time", text: $task.time)
                 .autocapitalization(.none)
                 .padding()
                 .cornerRadius(8)
                 .foregroundColor(.white)
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
         }
         .padding()
         .frame(width: 380.0, height: 40.0)
@@ -41,7 +67,7 @@ struct TaskListRowView: View {
     @Binding var taskList: TaskList
 
     var body: some View {
-        TextField("ritual", text: $taskList.name)
+        TextField("Ritual", text: $taskList.name)
             .autocapitalization(.none)
             .font(.title2)
             .padding()
@@ -63,7 +89,7 @@ struct TaskListView: View {
             }
 
             Button(action: addTask) {
-                Text("add task")
+                Text("Add Task")
                     .foregroundColor(.white)
                     .background(Color.black)
                     .cornerRadius(8)
@@ -79,13 +105,7 @@ struct TaskListView: View {
 }
 
 struct RitualView: View {
-    @State private var taskLists: [TaskList] = [
-        TaskList(name: "morning", tasks: [
-            Task(name: "stretch", time: "8:00-8:10am"),
-            Task(name: "shower", time: "8:10-8:40am")
-        ])
-    ]
-
+    @ObservedObject var manager = TaskListManager()
     @State private var expandedSection: UUID?
 
     var body: some View {
@@ -95,34 +115,37 @@ struct RitualView: View {
                     .ignoresSafeArea()
                     .blur(radius: 4.0)
 
-                VStack(spacing: 20) {
-                    Text("user's rituals")
+                VStack {
+                    Text("User's Rituals")
                         .fontWeight(.light)
                         .font(.title)
-                        .offset(y: 30)
-
+                        .padding(.top, 50)
+                    
                     ScrollView {
-                        ForEach(taskLists.indices, id: \.self) { index in
-                            VStack {
-                                TaskListRowView(taskList: $taskLists[index])
-                                    .onTapGesture {
-                                        withAnimation {
-                                            if self.expandedSection == taskLists[index].id {
-                                                self.expandedSection = nil
-                                            } else {
-                                                self.expandedSection = taskLists[index].id
+                        VStack(spacing: 20) {
+                            ForEach(manager.taskLists.indices, id: \.self) { index in
+                                VStack {
+                                    TaskListRowView(taskList: $manager.taskLists[index])
+                                        .onTapGesture {
+                                            withAnimation {
+                                                if expandedSection == manager.taskLists[index].id {
+                                                    expandedSection = nil
+                                                } else {
+                                                    expandedSection = manager.taskLists[index].id
+                                                }
                                             }
                                         }
+                                    if expandedSection == manager.taskLists[index].id {
+                                        TaskListView(tasks: $manager.taskLists[index].tasks)
+                                            .transition(.move(edge: .top))
                                     }
-
-                                if self.expandedSection == taskLists[index].id {
-                                    TaskListView(tasks: $taskLists[index].tasks)
-                                        .transition(.move(edge: .top))
                                 }
                             }
-                            .padding()
                         }
+                        .padding(.horizontal)
                     }
+                    
+                    Spacer()
                 }
             }
             .navigationBarItems(trailing: addButton)
@@ -131,8 +154,9 @@ struct RitualView: View {
 
     var addButton: some View {
         Button(action: {
-            if taskLists.count < 8 {
-                taskLists.append(TaskList(name: "ritual", tasks: []))
+            if manager.taskLists.count < 8 {
+                manager.taskLists.append(TaskList(name: "Ritual", tasks: []))
+                expandedSection = manager.taskLists.last?.id
             }
         }) {
             Image(systemName: "plus")
