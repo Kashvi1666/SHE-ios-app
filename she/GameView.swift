6624663ee559e96fcf10143088ebdf5f6784e261
kashvi <kashviswami@gmail.com>
2023-08-17 21:33:07 -0700

    move reflection view files

--- she/GameView.swift
+++ she/GameView.swift
@@ -1,8 +1,19 @@
-//
 //  GameView.swift
-//  she
-//
 //  Created by Kashvi Swami on 8/16/23.
-//
 
 import Foundation
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        NavigationStack {
+            Image("background").ignoresSafeArea()
+        }
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
