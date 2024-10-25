33a88135da54a0c1fe919789f8708eaa4f5260f1
kashvi <79034048+Kashvi1666@users.noreply.github.com>
2024-10-25 18:01:54 -0400

    Update sheUITests.swift

--- sheUITests/sheUITests.swift
+++ sheUITests/sheUITests.swift
@@ -10,29 +10,25 @@ import XCTest
 final class sheUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // setup code here
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // method called after each test method in the class
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
+        // UI tests launch the application 
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // XCTAssert and related functions to verify results
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
+            // measures time to launch 
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
